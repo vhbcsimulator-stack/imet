1b54f64d9fe6aa5b18369440b55343cc13894fd0
vhbcsimulator-stack <vhbcsimulator@gmail.com>
2026-01-07 15:50:08 +0800

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1771332673"
+    serviceWorkerVersion: "3145331710"
   }
 });
